b833cbb360d0133f2e64c0c785ecc60ae76d9dc8
sangwooshin <jsw7460@gmail.com>
2026-04-15 08:44:05 -0500

    Add Booster T1 robot config to asset_zoo
    Add booster_t1 directory with t1.xml, mesh assets, and t1_constants.py,
    and register T1_ACTION_SCALE / get_t1_robot_cfg in asset_zoo.robots.

--- src/mjlab/asset_zoo/robots/__init__.py
+++ src/mjlab/asset_zoo/robots/__init__.py
@@ -22,3 +22,9 @@ from mjlab.asset_zoo.robots.unitree_go2.go2_constants import (
 from mjlab.asset_zoo.robots.unitree_go2.go2_constants import (
   get_go2_robot_cfg as get_go2_robot_cfg,
 )
+from mjlab.asset_zoo.robots.booster_t1.t1_constants import (
+  T1_ACTION_SCALE as T1_ACTION_SCALE,
+)
+from mjlab.asset_zoo.robots.booster_t1.t1_constants import (
+  get_t1_robot_cfg as get_t1_robot_cfg,
+)
